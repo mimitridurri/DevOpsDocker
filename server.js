5c9a8da49f226524690ee85093c17ef63875395c
mimitridurri <145472188+mimitridurri@users.noreply.github.com>
2024-05-09 11:34:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.get('/', (req, res) => {
     res.json({
         message1: 'Hello FS2024 DevOps Course! These changes are made by murridim',
-        message2: 'Weitere Änderungen um Verknüpfung der Jobs zu prüfen'
+        message2: 'Weitere Änderungen um Verknüpfung der Jobs zu prüfen',
         message3: 'Weitere Änderungen um SCM Prüfungsintervall zu prüfen'
     });
 });
